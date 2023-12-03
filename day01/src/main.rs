c99c6d2fa49115db7b7a3ba6ce118d30e78565dc
Zach Thompson <zach@allotropic.com>
2023-12-03 05:46:22 -0500

    Solve day01_1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let result = include_str!("input.txt").lines().fold(0, |acc, line| {
+        let mut digits = line.chars().filter(char::is_ascii_digit);
+
+        let first = digits.next().unwrap();
+        let last = digits.last().unwrap_or(first);
+
+        let number = first.to_digit(10).unwrap() * 10 + last.to_digit(10).unwrap();
+
+        acc + number
+    });
+
+    println!("{result}");
 }
