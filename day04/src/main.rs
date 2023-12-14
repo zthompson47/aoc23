f0aa931b3081b98d9da67abe59ce39de83bf5de6
Zach Thompson <zach@allotropic.com>
2023-12-14 07:43:31 -0500

    Solve day 4 part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,7 +1,8 @@
 use std::collections::BTreeSet;
 
 fn main() {
-    let result: u32 = include_str!("input.txt")
+    let mut scores = Vec::new();
+    let result_pt1: u32 = include_str!("input.txt")
         .lines()
         .map(|line| {
             let card = line
@@ -18,8 +19,10 @@ fn main() {
             let winners = &card[0];
             let picks = &card[1];
 
-            let result = picks.iter().fold(0, |game_score, x| {
+            let mut win_count = 0;
+            let score = picks.iter().fold(0, |game_score, x| {
                 if winners.contains(x) {
+                    win_count += 1;
                     if game_score == 0 {
                         1
                     } else {
@@ -30,9 +33,20 @@ fn main() {
                 }
             });
 
-            result
+            scores.push(win_count);
+
+            score
         })
         .sum();
 
-    println!("{result:?}");
+    println!("{result_pt1:?}");
+
+    let mut aggregate_scores = vec![1; scores.len()];
+
+    for i in 0..scores.len() {
+        for j in 0..scores[i] {
+            aggregate_scores[i + j + 1] += aggregate_scores[i];
+        }
+    }
+    println!("{}", aggregate_scores.iter().sum::<u32>());
 }
