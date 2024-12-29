71e44a7e2b3e19e2825fbf2a7eeeb08e0ec47e90
Zach Thompson <zach@allotropic.com>
2024-12-29 11:48:45 -0500

    Solve day 14 part 1

--- day14/main.rs
+++ day14/main.rs
@@ -1,9 +1,95 @@
+use std::collections::HashMap;
+
 fn main() {
-    let grid: Grid<Cell> = Grid::from(include_str!("test.txt"));
+    let mut grid: Grid<Cell> = Grid::from(include_str!("input.txt"));
     println!("{grid}");
+    grid.tilt(Direction::N);
+    println!("{grid}");
+
+    println!("Part 1: {}", grid.score());
 }
 
-#[derive(Debug)]
+impl Grid<Cell> {
+    fn score(&self) -> usize {
+        let mut score = 0;
+        for (i, row) in self.inner.iter().enumerate() {
+            let rank = self.dimensions().r - i;
+            score += row.iter().filter(|x| **x == Cell::Round).count() * rank;
+        }
+        score
+    }
+
+    fn tilt(&mut self, direction: Direction) {
+        if [Direction::N, Direction::S].contains(&direction) {
+            for column in 0..self.dimensions().c {
+                self.tilt_column(column, direction);
+            }
+        } else {
+            for column in 0..self.dimensions().r {
+                self.tilt_row(column, direction);
+            }
+        }
+    }
+
+    fn tilt_column(&mut self, column: usize, direction: Direction) {
+        // Scan for round rocks and store pile-up points at cube rocks.
+        let mut cubes: HashMap<usize, usize> = HashMap::new();
+        let mut rounds = 0;
+        for mut row in 0..self.dimensions().r {
+            row = match direction {
+                Direction::N => self.dimensions().r - row - 1,
+                Direction::S => todo!(),
+                _ => unreachable!(),
+            };
+            match self.inner[row][column] {
+                Cell::Round => {
+                    rounds += 1;
+                    self.inner[row][column] = Cell::Empty;
+                }
+                Cell::Cube => {
+                    if rounds > 0 {
+                        cubes.insert(row + 1, rounds);
+                        rounds = 0;
+                    }
+                }
+                Cell::Empty => {}
+            }
+        }
+        match direction {
+            Direction::N => cubes.insert(0, rounds),
+            Direction::S => todo!(),
+            _ => unreachable!(),
+        };
+
+        // Scan for cube rocks and insert piled-up round rocks behind each.
+        for (start, rounds) in cubes {
+            let range = match direction {
+                Direction::N => start..start + rounds,
+                Direction::S => todo!(),
+                _ => unreachable!(),
+            };
+            for row in range {
+                self.inner[row][column] = Cell::Round;
+            }
+        }
+    }
+
+    #[allow(unused_variables)]
+    fn tilt_row(&mut self, row: usize, direction: Direction) {
+        todo!()
+    }
+}
+
+#[allow(dead_code)]
+#[derive(PartialEq, Clone, Copy)]
+enum Direction {
+    N,
+    E,
+    S,
+    W,
+}
+
+#[derive(Debug, PartialEq)]
 enum Cell {
     Round,
     Cube,
@@ -40,6 +126,18 @@ impl<T> Grid<T> {
     fn new() -> Self {
         Grid { inner: Vec::new() }
     }
+
+    fn dimensions(&self) -> Dimensions {
+        Dimensions {
+            r: self.inner.len(),
+            c: self.inner[0].len(),
+        }
+    }
+}
+
+struct Dimensions {
+    r: usize,
+    c: usize,
 }
 
 impl<T> std::fmt::Display for Grid<T>
