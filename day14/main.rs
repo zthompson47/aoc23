83b83096e5f285749e45aa8a1deab3a92fc64b4e
Zach Thompson <zach@allotropic.com>
2024-12-29 13:48:41 -0500

    Solve day 14 part 2

--- day14/main.rs
+++ day14/main.rs
@@ -2,11 +2,46 @@ use std::collections::HashMap;
 
 fn main() {
     let mut grid: Grid<Cell> = Grid::from(include_str!("input.txt"));
-    println!("{grid}");
     grid.tilt(Direction::N);
-    println!("{grid}");
-
     println!("Part 1: {}", grid.score());
+
+    // Spin the grid until we find it cycled back to a prior state, then calculate
+    // final state from modulo.
+    let mut grid: Grid<Cell> = Grid::from(include_str!("test.txt"));
+    let mut cycle_cache: Vec<Grid<Cell>> = Vec::new();
+    let mut repeat = None;
+    let cycles = 1_000_000_000;
+    for i in 0..cycles {
+        grid.tilt(Direction::N);
+        grid.tilt(Direction::W);
+        grid.tilt(Direction::S);
+        grid.tilt(Direction::E);
+        if cycle_cache.contains(&grid) {
+            repeat = Some(i);
+            cycle_cache.push(grid.clone());
+            break;
+        }
+        cycle_cache.push(grid.clone());
+    }
+    if let Some(repeat) = repeat {
+        let first_repeat = cycle_cache
+            .iter()
+            .position(|x| x == cycle_cache.last().unwrap())
+            .unwrap();
+        println!("first_repeat:{first_repeat}, len cache: {}", cycle_cache.len());
+        for grid in &cycle_cache {
+            println!("-->> {}", grid.score());
+        }
+        let index = (cycles - first_repeat - 1) % (cycle_cache.len() - first_repeat - 1);
+        println!("index -->> {index}");
+        grid = cycle_cache[first_repeat..cycle_cache.len() - 1][index].clone();
+
+        //let index = (cycles - first_repeat) % (cycle_cache.len() - first_repeat - 2) + first_repeat;
+        //grid = cycle_cache[first_repeat..][index].clone();
+        //let index = (cycles - first_repeat) % (cycle_cache.len() - 1 - first_repeat);
+        //grid = cycle_cache[first_repeat..cycle_cache.len() - 2][index].clone();
+    }
+    println!("Part 2: {}", grid.score());
 }
 
 impl Grid<Cell> {
@@ -38,7 +73,7 @@ impl Grid<Cell> {
         for mut row in 0..self.dimensions().r {
             row = match direction {
                 Direction::N => self.dimensions().r - row - 1,
-                Direction::S => todo!(),
+                Direction::S => row,
                 _ => unreachable!(),
             };
             match self.inner[row][column] {
@@ -48,7 +83,15 @@ impl Grid<Cell> {
                 }
                 Cell::Cube => {
                     if rounds > 0 {
-                        cubes.insert(row + 1, rounds);
+                        cubes.insert(
+                            (row as isize
+                                + match direction {
+                                    Direction::N => 1,
+                                    Direction::S => -1,
+                                    _ => unreachable!(),
+                                }) as usize,
+                            rounds,
+                        );
                         rounds = 0;
                     }
                 }
@@ -57,7 +100,7 @@ impl Grid<Cell> {
         }
         match direction {
             Direction::N => cubes.insert(0, rounds),
-            Direction::S => todo!(),
+            Direction::S => cubes.insert(self.dimensions().r - 1, rounds),
             _ => unreachable!(),
         };
 
@@ -65,7 +108,7 @@ impl Grid<Cell> {
         for (start, rounds) in cubes {
             let range = match direction {
                 Direction::N => start..start + rounds,
-                Direction::S => todo!(),
+                Direction::S => (start as isize - rounds as isize + 1) as usize..start + 1,
                 _ => unreachable!(),
             };
             for row in range {
@@ -74,9 +117,55 @@ impl Grid<Cell> {
         }
     }
 
-    #[allow(unused_variables)]
     fn tilt_row(&mut self, row: usize, direction: Direction) {
-        todo!()
+        // Scan for round rocks and store pile-up points at cube rocks.
+        let mut cubes: HashMap<usize, usize> = HashMap::new();
+        let mut rounds = 0;
+        for mut column in 0..self.dimensions().c {
+            column = match direction {
+                Direction::W => self.dimensions().c - column - 1,
+                Direction::E => column,
+                _ => unreachable!(),
+            };
+            match self.inner[row][column] {
+                Cell::Round => {
+                    rounds += 1;
+                    self.inner[row][column] = Cell::Empty;
+                }
+                Cell::Cube => {
+                    if rounds > 0 {
+                        cubes.insert(
+                            (column as isize
+                                + match direction {
+                                    Direction::W => 1,
+                                    Direction::E => -1,
+                                    _ => unreachable!(),
+                                }) as usize,
+                            rounds,
+                        );
+                        rounds = 0;
+                    }
+                }
+                Cell::Empty => {}
+            }
+        }
+        match direction {
+            Direction::W => cubes.insert(0, rounds),
+            Direction::E => cubes.insert(self.dimensions().c - 1, rounds),
+            _ => unreachable!(),
+        };
+
+        // Scan for cube rocks and insert piled-up round rocks behind each.
+        for (start, rounds) in cubes {
+            let range = match direction {
+                Direction::W => start..start + rounds,
+                Direction::E => (start as isize - rounds as isize + 1) as usize..start + 1,
+                _ => unreachable!(),
+            };
+            for column in range {
+                self.inner[row][column] = Cell::Round;
+            }
+        }
     }
 }
 
@@ -89,7 +178,7 @@ enum Direction {
     W,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Cell {
     Round,
     Cube,
@@ -117,12 +206,18 @@ impl From<&Cell> for char {
     }
 }
 
-#[derive(Debug)]
-struct Grid<T> {
+#[derive(Debug, Clone, PartialEq)]
+struct Grid<T>
+where
+    T: Clone,
+{
     inner: Vec<Vec<T>>,
 }
 
-impl<T> Grid<T> {
+impl<T> Grid<T>
+where
+    T: Clone,
+{
     fn new() -> Self {
         Grid { inner: Vec::new() }
     }
@@ -143,6 +238,7 @@ struct Dimensions {
 impl<T> std::fmt::Display for Grid<T>
 where
     char: for<'a> std::convert::From<&'a T>,
+    T: Clone,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for row in self.inner.iter() {
@@ -155,6 +251,7 @@ where
 impl<T> From<&str> for Grid<T>
 where
     T: From<char>,
+    T: Clone,
 {
     fn from(value: &str) -> Self {
         value.lines().fold(Grid::new(), |mut grid, row| {
