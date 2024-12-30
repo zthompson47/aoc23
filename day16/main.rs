031bc97d6198ec1f19815644378d8017c9a94e06
Zach Thompson <zach@allotropic.com>
2024-12-30 10:05:41 -0500

    Clean up day 16 part 1

--- day16/main.rs
+++ day16/main.rs
@@ -28,57 +28,6 @@ fn main() {
         }
     }
 
-    /*
-    use colored::Colorize;
-    let mut count = 0;
-    for r in 0..visited.len() {
-        for c in 0..visited[0].len() {
-            if visited[r][c].is_empty() {
-                print!("{}", char::from(&grid.cell(Position { r, c })));
-            } else if visited[r][c].len() > 2 {
-                count += 1;
-                print!(
-                    "{}",
-                    char::from(&grid.cell(Position { r, c }))
-                        .to_string()
-                        .as_str()
-                        .purple()
-                        .reversed()
-                );
-            } else if visited[r][c].len() > 1 {
-                count += 1;
-                print!(
-                    "{}",
-                    char::from(&grid.cell(Position { r, c }))
-                        .to_string()
-                        .as_str()
-                        .green()
-                        .reversed()
-                );
-            } else if visited[r][c].len() > 0 {
-                count += 1;
-                let cell = match visited[r][c][0] {
-                    Direction::N => '^',
-                    Direction::S => 'v',
-                    Direction::E => '>',
-                    Direction::W => '<',
-                };
-                print!(
-                    "{}",
-                    //cell
-                    char::from(&grid.cell(Position { r, c }))
-                        .to_string()
-                        .as_str()
-                        .red()
-                        .reversed()
-                );
-            }
-        }
-        println!();
-    }
-    println!("=========================={count}====================================");
-    */
-
     println!(
         "Part 1: {}",
         visited.iter().flatten().filter(|x| !x.is_empty()).count()
