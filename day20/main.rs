7a4ffdac1d83f2dd836c85781193bd48e69d6924
Zach Thompson <zach@allotropic.com>
2025-02-01 05:46:21 -0500

    Solve day 20 part 2

--- day20/main.rs
+++ day20/main.rs
@@ -3,141 +3,253 @@ use std::collections::HashMap;
 
 fn main() {
     let input = include_str!("input.txt");
+    //println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
 
-    // Part 1
-    let mut network = build_network(input);
-    let mut counter = Counter::default();
-    for _ in 0..1000 {
-        let mut messages = network
-            .get_mut("broadcaster")
-            .unwrap()
-            .send_signal(Signal::Low, "broadcaster");
-        counter.low += 1;
+fn part2(input: &'static str) -> u128 {
+    let mut network = Network::new(input);
+    let mut count = 0;
+
+    let first_nodes = ["ct", "hr", "ft", "qm"];
+    let initial_state = network.node_state(&first_nodes);
+
+    let (mut kd, mut zz, mut mh, mut cm): (Option<u128>, Option<u128>, Option<u128>, Option<u128>) =
+        (None, None, None, None);
+    'out: loop {
+        count += 1;
+
+        //if count % 100000 == 0 {
+        //println!("--> {count}");
+        //}
+
+        /*
+        println!(
+            //"{} {} {} {} {}",
+            "{}",
+            //network.node_state(&["js", "qs", "dt", "ts"]),
+            //network.node_state(&["zz"]),
+            network.node_state(&["mh"]),
+            //network.node_state(&["cm"]),
+            //network.node_state(&["kd"]),
+        );
+        */
+        //println!("{}", network.conjunction_state());
+        /*
+        println!(
+            "{} {}",
+            network.node_state(&["ct", "hr", "ft", "qm"]),
+            network.node_state(&["mh", "mj", "zz", "zq", "qn", "cm", "kb", "kd"]),
+        );
+        */
+
+        let to_cm = ["bd", "lg", "qd", "rp", "pc", "xl", "qn", "bk"];
+        let to_mh = ["mj", "fr", "tp", "fl", "tk", "jg", "xg", "lz", "ct"];
+        let to_zz = ["lj", "lh", "mp", "pz", "zq", "hr", "cx", "mf", "fs"];
+        let to_kd = ["ks", "gk", "vq", "xk", "kb", "qm", "hv", "nv", "qj"];
+
+        let zz_lineup = [
+            "mp", "mf", "pz", "lh", "fs", "jk", "cx", "lj", "bh", "th", "zq", "hr",
+        ];
+
+        //println!("{} {}", network.node_state(&zz_lineup), network.node_state(&["zz"]));
+
+        let mut messages = vec![Message {
+            from: "button",
+            to: "broadcaster",
+            signal: Signal::Low,
+        }];
+
         while !messages.is_empty() {
             let mut new_messages = vec![];
             for message in messages.iter() {
-                counter.count(message.signal);
-                if let Some(node) = network.get_mut(message.to) {
-                    new_messages.extend(node.send_signal(message.signal, message.from));
+                //if message.to == "cl" && message.signal == Signal::High {
+                //    println!("{count} {} {:?}", message.from, message.signal);
+                //}
+
+                //if message.to == "rx" && message.signal == Signal::Low {
+                //    break 'out count;
+                //}
+
+                if message.signal == Signal::Low {
+                    if message.to == "hr" && message.from == "zz" {
+                        zz = Some(count);
+                    }
+                    if message.to == "ct" && message.from == "mh" {
+                        mh = Some(count);
+                    }
+                    if message.to == "ft" && message.from == "cm" {
+                        cm = Some(count);
+                    }
+                    if message.to == "qm" && message.from == "kd" {
+                        kd = Some(count);
+                    }
+                    if let (Some(zz), Some(mh), Some(cm), Some(kd)) = (zz, mh, cm, kd) {
+                        println!("{zz} {mh} {cm} {kd}");
+                        use num::integer::lcm;
+                        break 'out lcm(zz, lcm(mh, lcm(cm, kd)));
+                    }
                 }
+
+                new_messages.extend(network.send_message(message));
+            }
+            messages = new_messages;
+        }
+
+        //if network.node_state(&first_nodes) == initial_state {
+        //println!("{}", network.node_state(&first_nodes));
+        //}
+    }
+}
+
+fn part1(input: &'static str) -> u32 {
+    let mut network = Network::new(input);
+    for _ in 0..1000 {
+        let mut messages = vec![Message {
+            from: "button",
+            to: "broadcaster",
+            signal: Signal::Low,
+        }];
+        while !messages.is_empty() {
+            let mut new_messages = vec![];
+            for message in messages.iter() {
+                new_messages.extend(network.send_message(message));
             }
             messages = new_messages;
         }
     }
-
-    println!("Part 1: {}", counter.high * counter.low);
-
-    /*
-    dbg!(&network);
-
-    // Part 2
-    let mut network = build_network(input);
-    let mut count = 0;
-    let count = 'out: loop {
-        let mut messages = network
-            .get_mut("broadcaster")
-            .unwrap()
-            .send_signal(Signal::Low, "broadcaster");
-        count += 1;
-        while !messages.is_empty() {
-            let mut new_messages = vec![];
-            for message in messages.iter() {
-                if message.to == "cl" && message.signal == Signal::High {
-                    println!("{count} {} {:?}", message.from, message.signal);
-                }
-
-                if message.to == "rx" && message.signal == Signal::Low {
-                    break 'out count;
-                }
-                if let Some(node) = network.get_mut(message.to) {
-                    new_messages.extend(node.send_signal(message.signal, message.from));
-                }
-            }
-            messages = new_messages;
-        }
-        /*
-        //if count % 10000000 == 0 {
-            //println!("{count}");
-            for node in network.values()
-            //.filter(|x| matches!(x.module, Module::Conjunction { .. }))
-            {
-                if let Module::Conjunction(signals) = &node.module {
-                    for signal in signals {
-                        print!(
-                            "{}",
-                            match signal.1 {
-                                Signal::Low => 0,
-                                Signal::High => 1,
-                            }
-                        );
-                    }
-                } else if let Module::FlipFlop(on) = &node.module {
-                    print!("{}", if *on { "1" } else { "0" });
-                }
-            }
-            println!();
-        //}
-        */
-    };
-
-    println!("Part 2: {count}");
-    */
+    network.counter.high * network.counter.low
 }
 
+#[derive(Debug)]
 struct Network {
     nodes: HashMap<&'static str, Node>,
     counter: Counter,
-    cycles: u32,
 }
 
-fn build_network(input: &'static str) -> HashMap<&'static str, Node> {
-    let mut module_inputs: HashMap<&'static str, Vec<&'static str>> = HashMap::new();
-    let mut network = input.lines().fold(HashMap::new(), |mut network, line| {
-        let (module, outputs) = line.split_once(" -> ").unwrap();
-        let outputs: Vec<&'static str> = outputs.split(", ").collect();
-        let (module_type, mut name) = module.split_at(1);
-        if module_type == "b" {
-            name = "broadcaster";
-        }
-
-        for output in &outputs {
-            module_inputs
-                .entry(output)
-                .and_modify(|inputs| inputs.push(name))
-                .or_insert(vec![name]);
-        }
-
-        network.insert(
-            name,
-            Node {
-                name,
-                module: match module_type {
-                    "b" => Module::Broadcaster,
-                    "%" => Module::FlipFlop(false),
-                    "&" => Module::Conjunction(HashMap::new()),
-                    _ => unreachable!(),
-                },
-                outputs,
-            },
-        );
-
-        network
-    });
-
-    // Store inputs for all `Module::Conjunction`.
-    for (name, inputs) in module_inputs {
-        if let Some(Node {
-            module: Module::Conjunction(input_signals),
-            ..
-        }) = network.get_mut(name)
-        {
-            for input in inputs {
-                input_signals.insert(input, Signal::Low);
+impl Network {
+    fn flip_flop_state(&self) -> String {
+        let mut result = String::new();
+        for node in self.nodes.values() {
+            if let Module::FlipFlop(on) = &node.module {
+                result.push_str(if *on { "1" } else { "0" });
             }
+        }
+        result
+    }
+
+    fn conjunction_state(&self) -> String {
+        let mut result = String::new();
+        for node in self.nodes.values() {
+            if let Module::Conjunction(signals) = &node.module {
+                for signal in signals {
+                    result.push_str(match signal.1 {
+                        Signal::Low => "0",
+                        Signal::High => "1",
+                    });
+                }
+            }
+        }
+        result
+    }
+
+    fn state(&self) -> String {
+        let mut result = String::new();
+        for node in self.nodes.values() {
+            if let Module::Conjunction(signals) = &node.module {
+                for signal in signals {
+                    result.push_str(match signal.1 {
+                        Signal::Low => "0",
+                        Signal::High => "1",
+                    });
+                }
+            } else if let Module::FlipFlop(on) = &node.module {
+                result.push_str(if *on { "1" } else { "0" });
+            }
+        }
+        result
+    }
+
+    fn node_state(&self, nodes: &[&'static str]) -> String {
+        let mut result = String::new();
+        for name in nodes {
+            if let Some(node) = self.nodes.get(name) {
+                if let Module::Conjunction(signals) = &node.module {
+                    for signal in signals {
+                        result.push_str(match signal.1 {
+                            Signal::Low => "0",
+                            Signal::High => "1",
+                        });
+                    }
+                } else if let Module::FlipFlop(on) = &node.module {
+                    result.push_str(if *on { "1" } else { "0" });
+                }
+            }
+        }
+        result
+    }
+
+    fn send_message(&mut self, message: &Message) -> Vec<Message> {
+        self.counter.count(message.signal);
+        if let Some(node) = self.nodes.get_mut(message.to) {
+            node.send_signal(message.signal, message.from)
+        } else {
+            vec![]
         }
     }
 
-    network
+    fn new(input: &'static str) -> Self {
+        let mut module_inputs: HashMap<&'static str, Vec<&'static str>> = HashMap::new();
+        let mut nodes = input.lines().fold(HashMap::new(), |mut nodes, line| {
+            let (module, outputs) = line.split_once(" -> ").unwrap();
+            let outputs: Vec<&'static str> = outputs.split(", ").collect();
+            let (module_type, mut name) = module.split_at(1);
+            if module_type == "b" {
+                name = "broadcaster";
+            }
+
+            for output in &outputs {
+                module_inputs
+                    .entry(output)
+                    .and_modify(|inputs| inputs.push(name))
+                    .or_insert(vec![name]);
+            }
+
+            nodes.insert(
+                name,
+                Node {
+                    name,
+                    module: match module_type {
+                        "b" => Module::Broadcaster,
+                        "%" => Module::FlipFlop(false),
+                        "&" => Module::Conjunction(HashMap::new()),
+                        _ => unreachable!(),
+                    },
+                    outputs,
+                },
+            );
+
+            nodes
+        });
+
+        // Store inputs for all `Module::Conjunction`.
+        for (name, inputs) in module_inputs {
+            if let Some(Node {
+                module: Module::Conjunction(input_signals),
+                ..
+            }) = nodes.get_mut(name)
+            {
+                for input in inputs {
+                    input_signals.insert(input, Signal::Low);
+                }
+            }
+        }
+
+        Network {
+            nodes,
+            counter: Counter::default(),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -229,55 +341,3 @@ impl Module {
         }
     }
 }
-
-/*
-trait Pulse {
-    fn pulse(&mut self, signal: Signal, source: &'static str) -> Option<Signal>;
-}
-
-struct Conjunction {
-    inputs: HashMap<&'static str, Signal>,
-}
-
-impl Pulse for Conjunction {
-    fn pulse(&mut self, signal: Signal, source: &'static str) -> Option<Signal> {
-        *self.inputs.get_mut(source).unwrap() = signal;
-        if self.inputs.values().any(|f| *f == Signal::Low) {
-            Some(Signal::High)
-        } else {
-            Some(Signal::Low)
-        }
-    }
-}
-
-struct FlipFlop {
-    on: bool,
-}
-
-impl Pulse for FlipFlop {
-    fn pulse(&mut self, signal: Signal, _: &'static str) -> Option<Signal> {
-        if signal == Signal::Low {
-            match self.on {
-                true => {
-                    self.on = false;
-                    Some(Signal::Low)
-                }
-                false => {
-                    self.on = true;
-                    Some(Signal::High)
-                }
-            }
-        } else {
-            None
-        }
-    }
-}
-
-struct Broadcaster;
-
-impl Pulse for Broadcaster {
-    fn pulse(&mut self, signal: Signal, _: &'static str) -> Option<Signal> {
-        Some(signal)
-    }
-}
-*/
